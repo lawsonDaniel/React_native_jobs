411e82e6ce34a77dbb98de3d0fe26cd06fb6dbcb
Lawson Daniel <lawblaze4@gmail.com>
2023-03-30 18:13:05 +0100

    done with the structure of the head

--- app/index.js
+++ app/index.js
@@ -1,8 +1,35 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React,{useState} from 'react'
+import {View, ScrollView,SafeAreaView} from 'react-native'
+import {Stack,useRouter} from 'expo-router'
+import {COLORS,icons,images,SIZEs} from '../constants'
+import {Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from '../components'
 
 export default function Home() {
+  const router = useRouter()
   return (
-    <div>Home</div>
+    <SafeAreaView style={{
+      flex:1,
+      backgroundColor: COLORS.lightWhite,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+       <Stack.Screen 
+          options={{
+            headerStyle:{backgroundColor: COLORS.lightWhite},
+            headerShadowVisible:false,
+            headerLeft:()=>
+             (
+               <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+             )
+            ,
+            headerRight:()=>(
+              <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+            ),
+          }}
+       />
+
+      
+    </SafeAreaView>
   )
 }
